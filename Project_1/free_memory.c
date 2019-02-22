b2d92855d5d7a26644ded50b413f78f7a0da8ee8
AmoghShrikhande <amogh.shrikhande@colorado.edu>
2019-02-21 23:26:52 -0700

    Adding the functionality to block free_memory function if there is no memory allocated previously

--- Project_1/free_memory.c
+++ Project_1/free_memory.c
@@ -10,8 +10,17 @@ int *memory_start;
 
 void free_memory (int a, int b)
 {
+
+
+     if( memory_start != NULL )
+{
      memory_allocation_flag = 1;
      free (memory_start);    
      printf("\n The allocated memory is now freed \n");   
-     
+}
+
+else 
+{
+printf("\n You need to allocate memory before freeing it \n");
+}
 }
